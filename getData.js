97505c3933fb38832df3e4e1d68b9b47bab5e3ac
Renato Filho <renatosousafilho@gmail.com>
2021-02-23 08:26:31 -0300

    Mudando getData para usar then e catch

--- getData.js
+++ getData.js
@@ -3,12 +3,11 @@ const fs = require('fs').promises;
 const arquive = './crush.json';
 
 const getData = async () => {
-  const newData = await fs.readFile(arquive, 'utf8', (err, data) => {
-    if (err) {
+  const newData = await fs.readFile(arquive, 'utf8')
+    .then((data) => data)
+    .catch((err) => {
       console.error(`Não foi possível ler o arquivo ${arquive}\n Erro: ${err}`);
-    }
-    return data;
-  });
+    });
   return newData;
 };
 
